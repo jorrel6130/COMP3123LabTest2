e172ea14478e3050508f2f7c518677778e41d81b
Jorrel6130 <jorrel6130@gmail.com>
2024-11-28 12:43:25 -0500

    Basic layout of components and navigation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
+// Jorrel Tigbayan
+// 101329925
 import logo from './logo.svg';
 import './App.css';
+import Search from './components/Search'
+import Result from './components/Result'
+import Toronto from './components/Toronto'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>ReactJS Weather App</h1>
+      <h3>Jorrel Tigbayan</h3>
+      <h4>101329925</h4>
+      <BrowserRouter>
+        <nav>
+          <NavLink to='/search'> Search</NavLink>
+          <NavLink to='/toronto'> Toronto</NavLink>
+        </nav>
+          <Routes>
+            <Route path='/search' element= { <Search /> }/>
+            <Route path='/result' element= { <Result /> }/>
+            <Route path='/toronto' element= { <Toronto /> }/>
+          </Routes>
+        </BrowserRouter>
     </div>
   );
 }
